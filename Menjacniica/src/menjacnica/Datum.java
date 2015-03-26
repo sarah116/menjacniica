fc9e3e1ee9cd18855ec782c2aaa1c74be7bd6378
sarah116 <saraahadzic@gmail.com>
2015-03-26 19:29:27 +0100

    Kreiran projekat Menjacniica. Ubacene klase Datum i Valuta i generisani gettersi i settersi za njihove atribute.

---
+++ Menjacniica/src/menjacnica/Datum.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+public class Datum {
+	
+	public int prodajniKurs;
+	public int kupovniKurs;
+	public int srednjiKurs;
+	public int getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(int prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public int getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(int kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public int getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(int srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	
+
+}
---
+++ Menjacniica/src/menjacnica/Valuta.java
@@ -0,0 +1,21 @@
+package menjacnica;
+
+public class Valuta {
+	
+	public String imeValute;
+	public String skracenoIme;
+	public String getImeValute() {
+		return imeValute;
+	}
+	public void setImeValute(String imeValute) {
+		this.imeValute = imeValute;
+	}
+	public String getSkracenoIme() {
+		return skracenoIme;
+	}
+	public void setSkracenoIme(String skracenoIme) {
+		this.skracenoIme = skracenoIme;
+	}
+	
+
+}
