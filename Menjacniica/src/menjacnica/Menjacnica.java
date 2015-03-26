8caa1c1738e6a05bce3ef78d6c5ad7b99221bd37
sarah116 <sarah116>
2015-03-26 19:59:54 +0100

    Dodata klasa Menjacnica koja ce implementirati interfejs MenjacnicaInterfejs

---
+++ Menjacniica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,22 @@
+package menjacnica;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(String dan, int prodajni, int kupovni, int srednji) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void obrisiKurs(String dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int vratiKurs(String dan) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
